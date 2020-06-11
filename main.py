2c7b1a3fbf64f8cf6e727bd0a79046873210edb5
Matthew Foulk <mewuzhere12@gmail.com>
2020-06-11 11:44:02 -0400

    Seperated Data Backup

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 
-import spotify
-import database as db
 import os
 import sys
+
+import database as db
+import spotify
 
 ALREADY_BACKED_UP = 0
 
@@ -24,38 +25,7 @@ def main():
     if not cursor.fetchall():
         print("Creating tables...")
         db.initial_db_setup(cursor)
-    
-    print("Would you like to load your streaming history (Y or N)...")
-    load_history = input()
 
-    if load_history == 'Y':
-        mydata_check = os.path.exists("MyData")
-        streaming_history_check = os.path.exists("MyData/streaminghistory0.json")
-
-        if not mydata_check or not streaming_history_check:
-            print("Streaming history could not be found...")
-            print("Please download data from Spotify and add to this directory...")
-            sys.exit()
-        
-        print("Adding streaming history...")
-        streaming_history = spotify.get_streaming_history()
-        
-        total_tracks = 0
-
-        for file_data in streaming_history:
-            for count, track in enumerate(file_data):
-
-                # Skip over ones previously logged
-                total_tracks += 1
-                if total_tracks < ALREADY_BACKED_UP:
-                    print("Skipping songs already backed up...")
-                    continue
-
-                db.add_track_to_db(cursor, artist_names=[track["artistName"]], song_name=track["trackName"], played_at=track["endTime"])
-
-                if not count % 20:
-                    print("Commited up to #%i..." % total_tracks)
-                    conn.commit()
     print("Connecting to Spotify...")
     spotify_conn = spotify.connect_to_spotify(username)
 
